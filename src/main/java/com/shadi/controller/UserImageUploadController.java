ae2ccaeb62c6a90b87faeed585bec54c40ff1ad2
Hrushikesh <hrushikeshsurkar@gmial.com>
2024-11-01 14:33:31 +0530

    route update

--- src/main/java/com/shadi/controller/UserImageUploadController.java
+++ src/main/java/com/shadi/controller/UserImageUploadController.java
@@ -14,7 +14,7 @@ import com.shadi.response.DeleteImageResponse;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/images")
+@RequestMapping("/api/v1/images")
 public class UserImageUploadController {
     private final ImageUploadService imageUploadService;
 
